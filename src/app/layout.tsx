1a0f3d5323b136feee5f5da0a70541b34fb56851
amirhoseinkoosha <amirhoseinkoosha954@gmail.com>
2026-04-28 13:20:51 +0330

    remove beforeInteractive in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <head>
         <Script
           src="https://tapi.bale.ai/miniapp.js?3"
-          strategy="beforeInteractive"
+          // strategy="beforeInteractive"
         />
       </head>
       <body className="flex min-h-full flex-col">
